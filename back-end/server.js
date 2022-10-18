87a25c1b19e8f2f4d7b151db7d29d264d6216a49
Hind <hind.nayhi@gmail.com>
2022-10-18 14:20:27 -0600

    Clean up folders.

--- back-end/server.js
+++ back-end/server.js
@@ -1,19 +1,34 @@
-const Express = require('express');
-const App = Express();
-const BodyParser = require('body-parser');
+// load .env data into process.env
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const methodOverride = require('method-override');
 const PORT = 8080;
 
 // Express Configuration
-App.use(BodyParser.urlencoded({ extended: false }));
-App.use(BodyParser.json());
-App.use(Express.static('public'));
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Sample GET route
-App.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
 }));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
 
-App.listen(PORT, () => {
+// Routes
+
+
+// Mount all ressource routes
+
+
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+  console.log(`Express is be listening on port ${PORT}!`);
 });
