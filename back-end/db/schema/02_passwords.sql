c0c9bb62aea97e7df84bfbd759a75c5f80b86844
Hind <hind.nayhi@gmail.com>
2022-10-18 12:54:00 -0600

    Setup database and file to reset database.

---
+++ back-end/db/schema/02_passwords.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS passwords CASCADE;
+
+CREATE TABLE passwords (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  content VARCHAR(255) NOT NULL,
+  date_created DATE NOT NULL
+);
