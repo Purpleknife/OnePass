54582c90905ece3153c372c7935832bf81fa3a87
Hind <hind.nayhi@gmail.com>
2022-10-19 15:56:47 -0600

    Implement React Router in <App>.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import LandingPage from './components/LandingPage';
+
 
 const App = () => {
   return (
-    <div>
-      Hello!
-    </div>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+
+        </Routes>      
+      </BrowserRouter>
+    </React.StrictMode>
   );
 }
  
