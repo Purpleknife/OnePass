1e0525727f4d3788e60082e674a3a7edd66885f3
Hind <hind.nayhi@gmail.com>
2022-10-19 16:35:40 -0600

    Add routes for / and /dashboard.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import LandingPage from './components/LandingPage';
-
+import Dashboard from './components/Dashboard';
 
 const App = () => {
+  const [user, setUser] = useState();
+
   return (
     <React.StrictMode>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<LandingPage />} />
-
+          <Route path='/' element={<LandingPage user={user} setUser={setUser}/>} />
+          <Route path='/dashboard' element={<Dashboard />} />
         </Routes>      
       </BrowserRouter>
     </React.StrictMode>
