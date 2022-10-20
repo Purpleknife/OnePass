896b17138c4ce74f7f9d8f2fe3cae155169b75d4
Hind <hind.nayhi@gmail.com>
2022-10-20 17:04:43 -0600

    Implement code to verify if checkbox is checked.

--- front-end/src/components/GeneratePass.js
+++ front-end/src/components/GeneratePass.js
@@ -5,6 +5,25 @@ import './GeneratePass.scss';
 
 const GeneratePass = () => {
   const [length, setLength] = useState(0);
+  const [lowercaseIsChecked, setLowercaseIsChecked] = useState(false);
+  const [uppercaseIsChecked, setUppercaseIsChecked] = useState(false);
+  const [symbolsIsChecked, setSymbolsIsChecked] = useState(false);
+  const [numbersIsChecked, setNumbersIsChecked] = useState(false);
+
+  const handleOnChange = (condition) => {
+    if (condition === 'lowercase') {
+      setLowercaseIsChecked(!lowercaseIsChecked);
+    }
+    if (condition === 'uppercase') {
+      setUppercaseIsChecked(!uppercaseIsChecked);
+    }
+    if (condition === 'symbols') {
+      setSymbolsIsChecked(!symbolsIsChecked);
+    }
+    if (condition === 'numbers') {
+      setNumbersIsChecked(!numbersIsChecked);
+    }
+  };
 
   const increaseLength = () => {
     setLength(prev => prev + 1);
@@ -25,10 +44,38 @@ const GeneratePass = () => {
       
       <div className='options'>
         <span>Length: <i onClick={decreaseLength} className="fa-solid fa-square-minus"></i> {length} <i onClick={increaseLength} className="fa-solid fa-square-plus"></i></span>
-        <span><input type="checkbox" id="lowercase" name="lowercase" value="Lowercase" /> Lowercase</span>
-        <span><input type="checkbox" id="uppercase" name="uppercase" value="Uppercase" /> Uppercase</span>
-        <span><input type="checkbox" id="symbol" name="symbol" value="Symbols" /> Symbols</span>
-        <span><input type="checkbox" id="numbers" name="numbers" value="Numbers" /> Numbers</span>
+        <span><input 
+          type="checkbox"
+          id="lowercase"
+          name="lowercase" 
+          checked={lowercaseIsChecked}
+          onChange={handleOnChange(lowercase)}
+          value="Lowercase" /> Lowercase
+        </span>
+        <span><input 
+          type="checkbox" 
+          id="uppercase"
+          name="uppercase"
+          checked={uppercaseIsChecked}
+          onChange={handleOnChange(uppercase)}
+          value="Uppercase" /> Uppercase
+        </span>
+        <span><input 
+          type="checkbox"
+          id="symbol" 
+          name="symbol"
+          checked={symbolsIsChecked}
+          onChange={handleOnChange(symbols)}
+          value="Symbols" /> Symbols
+        </span>
+        <span><input 
+          type="checkbox" 
+          id="numbers" 
+          name="numbers"
+          checked={numbersIsChecked}
+          onChange={handleOnChange(numbers)} 
+          value="Numbers" /> Numbers
+        </span>
       </div>
 
       <button className='generate_btn'>Generate</button>
